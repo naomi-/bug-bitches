0fbb8c4f397f57e5de2782f702a99aabf29f8964
Naomi Touchet <Naomi Touchet>
2016-02-21 16:40:57 -0800

    charBug update: NOW A VERY CUTE GREEN BUG!

--- Bug.java
+++ Bug.java
@@ -25,7 +25,8 @@ public class Bug {
     bugsMade++;
     numberOfBugs++;
   }
-  
+
+// BEWARE OF CHAR-BUG! SHE WILL STING YOU!  
   public static void charBug(double x, double y){
     
 //hair
@@ -34,52 +35,24 @@ public class Bug {
     StdDraw.filledCircle(x, y + .2, .04);
     
 //body
-    StdDraw.setPenColor(StdDraw.YELLOW);
+    StdDraw.setPenColor(.341, .467, .173);
     StdDraw.filledCircle(x, y, .12);
     
 //face
-    StdDraw.setPenColor(.93, .84, .69);
+    StdDraw.setPenColor(1.0, .96, .93);
     StdDraw.filledCircle(x, (y+.13), .06);
     
 //eyes
-   StdDraw.setPenColor(StdDraw.BLACK);
-   StdDraw.filledCircle((x - .01), (y +.120), .01);
-   StdDraw.filledCircle((x + .03), (y + .120), .01);
+   StdDraw.setPenColor(.48, .25, .0);
+   StdDraw.filledCircle((x - .02), (y + .15), .01);
+   StdDraw.filledCircle((x + .02), (y + .15), .01);
 
 //mouth
-    //StdDraw.setPenColor(StdDraw.GREEN);
-    //StdDraw.filledCircle(.203, .13, .01);
-    
-// This is the javascript-ish version   
-////hair
-//    StdDraw.setPenColor(153, 124, 9);
-//    StdDraw.filledEllipse(200.0, 116.0, 90.0, 90.0);
-//    StdDraw.filledEllipse(200.0, 60.0, 40.0, 40.0);
-//    
-////body
-//    StdDraw.setPenColor(172, 145, 230);
-//    StdDraw.filledEllipse(200.0, 233.0, 200.0, 220.0);
-//    
-////legs
-//    StdDraw.filledRectangle(40.0, 180.0, 325.0, 20.0);
-//    StdDraw.filledRectangle(40.0, 220.0, 325.0, 20.0);
-//    StdDraw.filledRectangle(40.0, 260.0, 325.0, 20.0);
-//    
-////face
-//    StdDraw.setPenColor(242, 202, 175);
-//    StdDraw.filledEllipse(200.0, 116.0, 64.0, 55.0);
-//    
-////eyes
-//    StdDraw.setPenColor(26, 25, 26);
-//    StdDraw.filledEllipse(189.0, 116.0, 10.0, 10.0);
-//    StdDraw.filledEllipse(215.0, 116.0, 10.0, 10.0);
-//    
-////mouth
-//    StdDraw.setPenColor(199, 58, 58);
-//    StdDraw.filledEllipse(203.0, 130.0, 10.0, 10.0);
-    
+    StdDraw.setPenColor(StdDraw.PINK);
+    StdDraw.filledCircle(x , y + .106 , .012);    
   }
-  
+ 
+  // BEWARE OF NAOMI-BUG! SHE WILL BITE YOU!
   public static void nomBug(double x, double y){
     StdDraw.setPenColor(.78, .584, .671);
     StdDraw.filledCircle(x, y, .13);
